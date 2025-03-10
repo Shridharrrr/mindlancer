a67d3c80af60f043e08b6bab142e78bdfe4ba894
Shridhar Mandrekar <geekyshridhar04@gmail.com>
2025-03-10 12:28:26 +0530

    services added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import {
   ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
 } from '@clerk/nextjs'
 import "./globals.css";
 
